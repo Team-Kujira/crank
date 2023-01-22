b4dd122a44de4db6843a0ce17787dbf61006ca9d
codehans <94654388+codehans@users.noreply.github.com>
2023-01-22 18:34:05 +0000

    usk dd

--- src/workers/usk.ts
+++ src/workers/usk.ts
@@ -18,6 +18,7 @@ const DD: Record<string, number> = {
   kujira1eydneup86kyhew5zqt5r7tkxefr3w5qcsn3ssrpcw9hm4npt3wmqa7as3u: 4,
   kujira1fjews4jcm2yx7una77ds7jjjzlx5vgsessguve8jd8v5rc4cgw9s8rlff8: 12,
   kujira1f2jt3f9gzajp5uupeq6xm20h90uzy6l8klvrx52ujaznc8xu8d7s6av27t: 12,
+  kujira1twc28l5njc07xuxrs85yahy44y9lw5euwa7kpajc2zdh98w6uyksvjvruq: 12,
 
   kujira1hjyjafrt09p4hwsnwch29nrrs40lprfgesqdy44wnp27td872hsse2rree: 4,
   kujira1m4ves3ymz5hyrj3war3t7uxu9ewt8rwpunja87960n0gre3a5pzspgry4g: 12,
