d07c0ebe2962808cf3952759786c174a9c56a77b
codehans <94654388+codehans@users.noreply.github.com>
2023-02-26 20:41:23 +0000

    appsignal deploys

--- src/appsignal.ts
+++ src/appsignal.ts
@@ -2,4 +2,5 @@ import Appsignal from "@appsignal/javascript"; // For ES Module
 
 export const appsignal = new Appsignal.default({
   key: process.env.APPSIGNAL,
+  revision: process.env.RENDER_GIT_COMMIT,
 });
